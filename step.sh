1eb3fce37566d0ded4f420efb168f561e4a4c9fc
Tom Hu <88201630+thomasrockhu-codecov@users.noreply.github.com>
2021-08-05 12:50:06 -0400

    Update step.sh
    Co-authored-by: Mitchell Borrego <mitchell@codecov.io>

--- step.sh
+++ step.sh
@@ -4,9 +4,9 @@ set -e
 
 # Download and verify Codecov uploader
 curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --import # One-time step
-curl -Os https://uploader.codecov.io/latest/${OS}/codecov
-curl -Os https://uploader.codecov.io/latest/${OS}/codecov.SHA256SUM
-curl -Os https://uploader.codecov.io/latest/${OS}/codecov.SHA256SUM.sig
+curl -Os "https://uploader.codecov.io/latest/${OS}/codecov"
+curl -Os "https://uploader.codecov.io/latest/${OS}/codecov.SHA256SUM"
+curl -Os "https://uploader.codecov.io/latest/${OS}/codecov.SHA256SUM.sig"
 gpg --verify codecov.SHA256SUM.sig codecov.SHA256SUM
 
 shasum -a 256 -c codecov.SHA256SUM
