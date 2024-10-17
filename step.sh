2253d2f7e1e5bb327d26f20c03842020e4ba296e
Tom Hu <thomas.hu@sentry.io>
2024-10-17 17:08:34 -0500

    feat: migrate to wrapper and CLI

--- step.sh
+++ step.sh
@@ -1,79 +1,227 @@
-#!/bin/bash
-
-set -e
-
-CODECOV_PUBLIC_PGP_KEY="-----BEGIN PGP PUBLIC KEY BLOCK-----
-
-mQINBGCsMn0BEACiCKZOhkbhUjb+obvhH49p3ShjJzU5b/GqAXSDhRhdXUq7ZoGq
-KEKCd7sQHrCf16Pi5UVacGIyE9hS93HwY15kMlLwM+lNeAeCglEscOjpCly1qUIr
-sN1wjkd2cwDXS6zHBJTqJ7wSOiXbZfTAeKhd6DuLEpmA+Rz4Yc+4qZP+fVxVG3Pv
-2v06m+E5CP/JQVQPO8HYi+S36hJImTh+zaDspu+VujSai5KzJ6YKmgwslVNIp5X5
-GnEr2uAh5w6UTnt9UQUjFFliAvQ3lPLWzm7DWs6AP9hslYxSWzwbzVF5qbOIjUJL
-KfoUpvCYDs2ObgRn8WUQO0ndkRCBIxhlF3HGGYWKQaCEsiom7lyi8VbAszmUCDjw
-HdbQHFmm5yHLpTXJbg+iaxQzKnhWVXzye5/x92IJmJswW81Ky346VxYdC1XFL/+Y
-zBaj9oMmV7WfRpdch09Gf4TgosMzWf3NjJbtKE5xkaghJckIgxwzcrRmF/RmCJue
-IMqZ8A5qUUlK7NBzj51xmAQ4BtkUa2bcCBRV/vP+rk9wcBWz2LiaW+7Mwlfr/C/Q
-Swvv/JW2LsQ4iWc1BY7m7ksn9dcdypEq/1JbIzVLCRDG7pbMj9yLgYmhe5TtjOM3
-ygk25584EhXSgUA3MZw+DIqhbHQBYgrKndTr2N/wuBQY62zZg1YGQByD4QARAQAB
-tEpDb2RlY292IFVwbG9hZGVyIChDb2RlY292IFVwbG9hZGVyIFZlcmlmaWNhdGlv
-biBLZXkpIDxzZWN1cml0eUBjb2RlY292LmlvPokCTgQTAQoAOBYhBCcDTn/bhQ4L
-vCxi/4Brsortd5hpBQJgrDJ9AhsDBQsJCAcCBhUKCQgLAgQWAgMBAh4BAheAAAoJ
-EIBrsortd5hpxLMP/3Fbgx5EG7zUUOqPZ+Ya9z8JlZFIkh3FxYMfMFE8jH9Es26F
-V2ZTJLO259MxM+5N0XzObi3h4XqIzBn42pDRfwtojY5wl2STJ9Bzu+ykPog7OB1u
-yfWXDRKcqPTUIxI1/WdU+c0/WNE6wjyzK+lRc1YUlp4pdNU7l+j2vKN+jGi2b6nV
-PTPRsMcwy3B90fKf5h2wNMNqO+KX/rjgpG9Uhej+xyFWkGM1tZDQQYFj+ugQUj61
-BMsQrUmxOnaVVnix21cHnACDCaxqgQZH3iZyEOKPNMsRFRP+0fLEnUMP+DVnQE6J
-Brk1Z+XhtjGI9PISQVx5KKDKscreS/D5ae2Cw/FUlQMf57kir6mkbZVhz2khtccz
-atD0r59WomNywIDyk1QfAKV0+O0WeJg8A69/Jk6yegsrUb5qEfkih/I38vvI0OVL
-BYve/mQIHuQo5ziBptNytCrN5TXHXzguX9GOW1V1+3DR+w/vXcnz67sjlYDysf1f
-JUZv9edZ2RGKW7agbrgOw2hB+zuWZ10tjoEcsaSGOLtKRGFDfmu/dBxzl8yopUpa
-Tn79QKOieleRm5+uCcKCPTeKV0GbhDntCZJ+Yiw6ZPmrpcjDowAoMQ9kiMVa10+Q
-WwwoaRWuqhf+dL6Q2OLFOxlyCDKVSyW0YF4Vrf3fKGyxKJmszAL+NS1mVcdxuQIN
-BGCsMn0BEADLrIesbpfdAfWRvUFDN+PoRfa0ROwa/JOMhEgVsowQuk9No8yRva/X
-VyiA6oCq6na7IvZXMxT7di4FWDjDtw5xHjbtFg336IJTGBcnzm7WIsjvyyw8kKfB
-8cvG7D2OkzAUF8SVXLarJ1zdBP/Dr1Nz6F/gJsx5+BM8wGHEz4DsdMRV7ZMTVh6b
-PaGuPZysPjSEw62R8MFJ1fSyDGCKJYwMQ/sKFzseNaY/kZVR5lq0dmhiYjNVQeG9
-HJ6ZCGSGT5PKNOwx/UEkT6jhvzWgfr2eFVGJTcdwSLEgIrJIDzP7myHGxuOiuCmJ
-ENgL1f7mzGkJ/hYXq1RWqsn1Fh2I9KZMHggqu4a+s3RiscmNcbIlIhJLXoE1bxZ/
-TfYZ9Aod6Bd5TsSMTZNwV2am9zelhDiFF60FWww/5nEbhm/X4suC9W86qWBxs3Kh
-vk1dxhElRjtgwUEHA5OFOO48ERHfR7COH719D/YmqLU3EybBgJbGoC/yjlGJxv0R
-kOMAiG2FneNKEZZihReh8A5Jt6jYrSoHFRwL6oJIZfLezB7Rdajx1uH7uYcUyIaE
-SiDWlkDw/IFM315NYFA8c1TCSIfnabUYaAxSLNFRmXnt+GQpm44qAK1x8EGhY633
-e5B4FWorIXx0tTmsVM4rkQ6IgAodeywKG+c2Ikd+5dQLFmb7dW/6CwARAQABiQI2
-BBgBCgAgFiEEJwNOf9uFDgu8LGL/gGuyiu13mGkFAmCsMn0CGwwACgkQgGuyiu13
-mGkYWxAAkzF64SVpYvY9nY/QSYikL8UHlyyqirs6eFZ3Mj9lMRpHM2Spn9a3c701
-0Ge4wDbRP2oftCyPP+p9pdUA77ifMTlRcoMYX8oXAuyE5RT2emBDiWvSR6hQQ8bZ
-WFNXal+bUPpaRiruCCUPD2b8Od1ftzLqbYOosxr/m5Du0uahgOuGw6zlGBJCVOo7
-UB2Y++oZ8P7oDGF722opepWQ+bl2a6TRMLNWWlj4UANknyjlhyZZ7PKhWLjoC6MU
-dAKcwQUdp+XYLc/3b00bvgju0e99QgHZMX2fN3d3ktdN5Q2fqiAi5R6BmCCO4ISF
-o5j10gGU/sdqGHvNhv5C21ibun7HEzMtxBhnhGmytfBJzrsj7GOReePsfTLoCoUq
-dFMOAVUDciVfRtL2m8cv42ZJOXtPfDjsFOf8AKJk40/tc8mMMqZP7RVBr9RWOoq5
-y9D37NfI6UB8rPZ6qs0a1Vfm8lIh2/k1AFECduXgftMDTsmmXOgXXS37HukGW7AL
-QKWiWJQF/XopkXwkyAYpyuyRMZ77oF7nuqLFnl5VVEiRo0Fwu45erebc6ccSwYZU
-8pmeSx7s0aJtxCZPSZEKZ3mn0BXOR32Cgs48CjzFWf6PKucTwOy/YO0/4Gt/upNJ
-3DyeINcYcKyD08DEIF9f5tLyoiD4xz+N23ltTBoMPyv4f3X/wCQ=
-=ch7z
------END PGP PUBLIC KEY BLOCK-----"
-
-# Download and verify Codecov uploader
-echo "${CODECOV_PUBLIC_PGP_KEY}" | gpg --no-default-keyring --import # One-time step
-curl -Os "https://cli.codecov.io/${VERSION}/${OS}/codecov"
-curl -Os "https://cli.codecov.io/${VERSION}/${OS}/codecov.SHA256SUM"
-curl -Os "https://cli.codecov.io/${VERSION}/${OS}/codecov.SHA256SUM.sig"
-gpg --verify codecov.SHA256SUM.sig codecov.SHA256SUM
-shasum -a 256 -c codecov.SHA256SUM
-
-chmod +x codecov
-
-curl -H "Accept: application/json" "https://cli.codecov.io/${OS}/${VERSION}" | grep -o '\"version\":\"v[0-9\.\_]\+\"' | head -1
-
-#create commit in codecov
-./codecov create-commit -t ${CODECOV_TOKEN}
-
-#create report
-./codecov create-report -t ${CODECOV_TOKEN}
-
-echo "-Z ${other_options}" "${@}"
-
-# Upload coverage to Codecov
-./codecov do-upload -t ${CODECOV_TOKEN} -Z ${other_options} "${@}"
+#!/usr/bin/env bash
+CC_WRAPPER_VERSION="0.0.18"
+say() {
+  echo -e "$1"
+}
+exit_if_error() {
+  say "$r==> $1$x"
+  if [ $CC_FAIL_ON_ERROR = true ];
+  then
+     say "$r    Exiting...$x"
+     exit 1;
+  fi
+}
+lower() {
+  echo $(echo $1 | sed 's/CC//' | sed 's/_/-/g' | tr '[:upper:]' '[:lower:]')
+}
+write_existing_args() {
+  if [ -n "$(eval echo \$$1)" ];
+  then
+    echo " -$(lower "$1") $(eval echo \$$1)"
+  fi
+}
+write_truthy_args() {
+  if [ "$(eval echo \$$1)" = "true" ] || [ "$(eval echo \$$1)" = "1" ];
+  then
+    echo " -$(lower $1)"
+  fi
+}
+b="\033[0;36m"  # variables/constants
+g="\033[0;32m"  # info/debug
+r="\033[0;31m"  # errors
+x="\033[0m"
+say "     _____          _
+    / ____|        | |
+   | |     ___   __| | ___  ___ _____   __
+   | |    / _ \\ / _\` |/ _ \\/ __/ _ \\ \\ / /
+   | |___| (_) | (_| |  __/ (_| (_) \\ V /
+    \\_____\\___/ \\__,_|\\___|\\___\\___/ \\_/
+                           $r Wrapper-$CC_WRAPPER_VERSION$x
+                                  "
+CC_VERSION="${CC_VERSION:-latest}"
+CC_FAIL_ON_ERROR="${CC_FAIL_ON_ERROR:-false}"
+if [ -n "$CC_BINARY" ];
+then
+  if [ -f "$CC_BINARY" ];
+  then
+    cc_filename=$CC_BINARY
+  else
+    exit_if_error "Could not find binary file $CC_BINARY"
+  fi
+else
+  family=$(uname -s | tr '[:upper:]' '[:lower:]')
+  cc_os="windows"
+  [[ $family == "darwin" ]] && cc_os="macos"
+  [[ $family == "linux" ]] && cc_os="linux"
+  [[ $cc_os == "linux" ]] && \
+    osID=$(grep -e "^ID=" /etc/os-release | cut -c4-)
+  [[ $osID == "alpine" ]] && cc_os="alpine"
+  [[ $(arch) == "aarch64" && $family == "linux" ]] && cc_os+="-arm64"
+  say "$g==>$x Detected $b${cc_os}$x"
+  export cc_os=${cc_os}
+  export cc_version=${CC_VERSION}
+  cc_filename="codecov"
+  [[ $cc_os == "windows" ]] && cc_filename+=".exe"
+  export cc_filename=${cc_filename}
+  [[ $cc_os == "macos" ]]  && \
+    ! command -v gpg 2>&1 >/dev/null && \
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install gpg
+  cc_url="https://cli.codecov.io"
+  cc_url="$cc_url/${CC_VERSION}"
+  cc_url="$cc_url/${cc_os}/${cc_filename}"
+  say "$g ->$x Downloading $b${cc_url}$x"
+  curl -Os $cc_url
+  say "$g==>$x Finishing downloading $b${cc_os}:${CC_VERSION}$x"
+  say " "
+fi
+if [ "$CC_SKIP_VALIDATION" = "true" ] || [ -n "$CC_BINARY" ];
+then
+  say "$r==>$x Bypassing validation as requested by user"
+else
+CC_PUBLIC_PGP_KEY=$(curl https://keybase.io/codecovsecurity/pgp_keys.asc)
+  echo "${CC_PUBLIC_PGP_KEY}"  | \
+    gpg --no-default-keyring --import
+  # One-time step
+  say "$g==>$x Verifying GPG signature integrity"
+  sha_url="https://cli.codecov.io"
+  sha_url="${sha_url}/${cc_version}/${cc_os}"
+  sha_url="${sha_url}/${cc_filename}.SHA256SUM"
+  say "$g ->$x Downloading $b${sha_url}$x"
+  say "$g ->$x Downloading $b${sha_url}.sig$x"
+  say " "
+  curl -Os "$sha_url"
+  curl -Os "${sha_url}.sig"
+  if ! gpg --verify "${cc_filename}.SHA256SUM.sig" "${cc_filename}.SHA256SUM";
+  then
+    exit_if_error "Could not verify signature. Please contact Codecov if problem continues"
+  fi
+  if ! (shasum -a 256 -c "${cc_filename}.SHA256SUM" || \
+    sha256sum -c "${cc_filename}.SHA256SUM");
+  then
+    exit_if_error "Could not verify SHASUM. Please contact Codecov if problem continues"
+  fi
+  say "$g==>$x CLI integrity verified"
+  say
+fi
+cc_cli_args=()
+cc_cli_args+=( $(write_existing_args CC_AUTO_LOAD_PARAMS_FROM) )
+cc_cli_args+=( $(write_existing_args CC_ENTERPRISE_URL) )
+cc_cli_args+=( $(write_existing_args CC_YML_PATH) )
+cc_cli_args+=( $(write_truthy_args CC_VERBOSE) )
+cc_cc_args=()
+cc_cc_args+=( $(write_truthy_args CC_FAIL_ON_ERROR) )
+cc_cc_args+=( $(write_existing_args CC_GIT_SERVICE) )
+cc_cc_args+=( $(write_existing_args CC_PARENT_SHA) )
+cc_cc_args+=( $(write_existing_args CC_PR) )
+cc_cc_args+=( $(write_existing_args CC_SHA) )
+cc_cc_args+=( $(write_existing_args CC_SLUG) )
+cc_create_report_args=()
+cc_cr_args+=( $(write_existing_args CC_CODE) )
+cc_cr_args+=( $(write_truthy_args CC_FAIL_ON_ERROR) )
+cc_cr_args+=( $(write_existing_args CC_GIT_SERVICE) )
+cc_cr_args+=( $(write_existing_args CC_PR) )
+cc_cr_args+=( $(write_existing_args CC_SHA) )
+cc_cr_args+=( $(write_existing_args CC_SLUG) )
+cc_du_args=()
+OLDIFS=$IFS;IFS=,
+cc_du_args+=( $(write_existing_args CC_BRANCH) )
+cc_du_args+=( $(write_existing_args CC_BUILD) )
+cc_du_args+=( $(write_existing_args CC_BUILD_URL) )
+cc_du_args+=( $(write_existing_args CC_CODE) )
+cc_du_args+=( $(write_existing_args CC_DIR) )
+cc_du_args+=( $(write_truthy_args CC_DISABLE_FILE_FIXES) )
+cc_du_args+=( $(write_truthy_args CC_DISABLE_SEARCH) )
+cc_du_args+=( $(write_truthy_args CC_DRY_RUN) )
+cc_du_args+=( $(write_existing_args CC_ENV) )
+if [ -n "$CC_EXCLUDES" ];
+then
+  for directory in $CC_EXCLUDES; do
+    cc_du_args+=( " --exclude " "$directory" )
+  done
+fi
+cc_du_args+=( $(write_truthy_args CC_FAIL_ON_ERROR) )
+if [ -n "$CC_FILES" ];
+then
+  for file in $CC_FILES; do
+    cc_du_args+=( " --file " "$file" )
+  done
+fi
+if [ -n "$CC_FLAGS" ];
+then
+  for flag in $CC_FLAGS; do
+    cc_du_args+=( " --flag " "$flag" )
+  done
+fi
+cc_du_args+=( $(write_existing_args CC_GCOV_ARGS) )
+cc_du_args+=( $(write_existing_args CC_GCOV_EXECUTABLE) )
+cc_du_args+=( $(write_existing_args CC_GCOV_IGNORE) )
+cc_du_args+=( $(write_existing_args CC_GCOV_INCLUDE) )
+cc_du_args+=( $(write_existing_args CC_GIT_SERVICE) )
+cc_du_args+=( $(write_truthy_args CC_HANDLE_NO_REPORTS_FOUND) )
+cc_du_args+=( $(write_existing_args CC_JOB_CODE) )
+cc_du_args+=( $(write_truthy_args CC_LEGACY) )
+cc_du_args+=( $(write_existing_args CC_NAME) )
+cc_du_args+=( $(write_existing_args CC_NETWORK_FILTER) )
+cc_du_args+=( $(write_existing_args CC_NETWORK_PREFIX) )
+cc_du_args+=( $(write_existing_args CC_NETWORK_ROOT_FOLDER) )
+if [ -n "$CC_PLUGINS" ];
+then
+  for plugin in $CC_PLUGINS; do
+    cc_du_args+=( " --plugin " "$plugin" )
+  done
+fi
+cc_du_args+=( $(write_existing_args CC_PR) )
+cc_du_args+=( $(write_existing_args CC_REPORT_TYPE) )
+cc_du_args+=( $(write_existing_args CC_SHA) )
+cc_du_args+=( $(write_existing_args CC_SLUG) )
+cc_du_args+=( $(write_existing_args CC_SWIFT_PROJECT) )
+IFS=$OLDIFS
+unset NODE_OPTIONS
+# See https://github.com/codecov/uploader/issues/475
+chmod +x $cc_filename
+if [ -n "$CC_TOKEN_VAR" ];
+then
+  token="$(eval echo \$$CC_TOKEN_VAR)"
+else
+  token="$(eval echo $CC_TOKEN)"
+fi
+say "$g ->$x Token of length ${#token} detected"
+token_str=""
+token_arg=()
+if [ -n "$token" ];
+then
+  token_str+=" -t <redacted>"
+  token_arg+=( " -t " "$token")
+fi
+say "$g==>$x Running create-commit"
+say "      $b./$cc_filename $(echo "${cc_cli_args[@]}") create-commit$token_str $(echo "${cc_cc_args[@]}")$x"
+if ! ./$cc_filename \
+  ${cc_cli_args[*]} \
+  create-commit \
+  ${token_arg[*]} \
+  ${cc_cc_args[*]};
+then
+  exit_if_error "Failed to create-commit"
+fi
+say " "
+say "$g==>$x Running create-report"
+say "      $b./$cc_filename $(echo "${cc_cli_args[@]}") create-report$token_str $(echo "${cc_cr_args[@]}")$x"
+if ! ./$cc_filename \
+  ${cc_cli_args[*]} \
+  create-report \
+  ${token_arg[*]} \
+  ${cc_cr_args[*]};
+then
+  exit_if_error "Failed to create-report"
+fi
+say " "
+say "$g==>$x Running do-upload"
+say "      $b./$cc_filename $(echo "${cc_cli_args[@]}") do-upload$token_str $(echo "${cc_du_args[@]}")$x"
+if ! ./$cc_filename \
+  ${cc_cli_args[*]} \
+  do-upload \
+  ${token_arg[*]} \
+  ${cc_du_args[*]};
+then
+  exit_if_error "Failed to upload"
+fi
